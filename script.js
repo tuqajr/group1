4b361a48ad15f01b110df7533ccd505612d157ad
anasalashqar <anasalashqar16@gmail.com>
2025-02-06 12:35:30 +0300

    added script

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 //tuqa
 
-let o=1;
-let l=2;
-console.log(o+l);
-
-
+let o = 1;
+let l = 2;
+console.log(o + l);
 
 //anas
 const w = 4;
 const z = 2;
-const g =0;
+const g = 0;
 console.log(w + z);
 
 // zaid code
@@ -18,3 +16,43 @@ let v2 = 2;
 
 console.log(v1 + v2);
 
+// document.addEventListener("DOMContentLoaded", () => {
+//   // fetch("http://localhost:8080/workout/table", {
+//   //   method: "POST",
+//   //   headers: {
+//   //     "Content-Type": "application/json",
+//   //   },
+//   //   body: JSON.stringify({
+//   //     fitnessClass: "Triceps and Biceps",
+//   //     imageUrl: "image.url.com",
+//   //     Trainer: "Mohammed",
+//   //     Duration: 60,
+//   //     Date: new Date(27, 2, 2025),
+//   //   }),
+//   // })
+//   //   .then((res) => {
+//   //     if (!res.ok) {
+//   //       throw new Error(`HTTP error! Status: ${res.status}`);
+//   //     }
+//   //     return res.json();
+//   //   })
+//   //   .then((data) => {
+//   //     console.log("Response:", data);
+//   //   })
+//   //   .catch((error) => {
+//   //     console.error("Error:", error);
+//   //   });
+
+//   fetch("http://localhost:8080/workout/table", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     });
+// });
