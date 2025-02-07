947427fe3020bb957c19956bd95f5afb74cc180b
Ahmad-Arabi <177164340+Ahmad-Arabi@users.noreply.github.com>
2025-02-07 16:37:19 +0300

    Added workout.html and it's styling in Ahmed&Zaid.css

--- script.js
+++ script.js
@@ -1,20 +1,86 @@
-//tuqa
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  const weekdays = document.querySelectorAll(".weekdays");
+  const cardContainer = document.querySelector(".card_container");
 
-let o=1;
-let l=2;
-console.log(o+l);
+  weekdays.forEach((day) => {
+    day.addEventListener("click", function (e) {
+      e.preventDefault();
 
+      // Remove active class from all weekdays
+      weekdays.forEach((weekday) => weekday.classList.remove("active"));
 
+      // Add active class to the clicked weekday
+      day.classList.add("active");
 
-//anas
-const w = 4;
-const z = 2;
-const g =0;
-console.log(w + z);
+      // Update card content based on the clicked weekday
+      updateCards(day.id);
+    });
+  });
 
-// zaid code
-let v1 = 1;
-let v2 = 2;
+  function updateCards(day) {
+    const cardData = {
+      sunday: {
+        img: "https://img.freepik.com/premium-vector/fit-people-doing-exercise_18591-41271.jpg",
+        instructor: "Justin Daniel",
+        duration: "30 minutes",
+      },
+      monday: {
+        img: "https://img.freepik.com/premium-vector/fit-people-doing-exercise_18591-41271.jpg",
+        instructor: "Jane Smith",
+        duration: "45 minutes",
+      },
+      tuesday: {
+        img: "https://img.freepik.com/premium-vector/fit-people-doing-exercise_18591-41271.jpg",
+        instructor: "Smith",
+        duration: "20 minutes",
+      },
+      wednesday: {
+        img: "https://img.freepik.com/premium-vector/fit-people-doing-exercise_18591-41271.jpg",
+        instructor: "Jane Smith",
+        duration: "25 minutes",
+      },
+      thursday: {
+        img: "https://img.freepik.com/premium-vector/fit-people-doing-exercise_18591-41271.jpg",
+        instructor: "Jane Smith",
+        duration: "45 minutes",
+      },
+      friday: {
+        img: "https://img.freepik.com/premium-vector/fit-people-doing-exercise_18591-41271.jpg",
+        instructor: "Jane Smith",
+        duration: "45 minutes",
+      },
+      saturday: {
+        img: "https://img.freepik.com/premium-vector/fit-people-doing-exercise_18591-41271.jpg",
+        instructor: "Jane Smith",
+        duration: "45 minutes",
+      },
+    };
 
-console.log(v1 + v2);
+    // Get data for the selected day
+    const cards = cardData[day];
+    cardContainer.innerHTML = "";
 
+    // Generate 10 cards in a loop
+    for (let i = 1; i <= 10; i++) {
+      const cardHTML = `
+                <div class="col">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="${cards.img}" class="img-fluid rounded" alt="..." />
+                            </div>
+                            <div class="col card_information">
+                                <p class="card-title">Fitness Class Name #${i}</p>
+                                <p class="card-text">
+                                    <small>By ${cards.instructor}, ${cards.duration}</small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+      cardContainer.insertAdjacentHTML("beforeend", cardHTML);
+    }
+  }
+});
